7d8b1cae8beddfc27289cac8c88e4157d2ffb366
unbroken2650 <unbroken2650@gmail.com>
2024-02-10 01:10:41 +0900

    db 반영 1차 완료

--- ssu-exchange/src/routes/Search.jsx
+++ ssu-exchange/src/routes/Search.jsx
@@ -4,16 +4,20 @@ import Title from "../components/Title";
 import School from "../components/School";
 
 export default function Search() {
-  const [universities, setUniversities] = useState(false);
+  const [universities, setUniversities] = useState([]);
+  const [isUnivLoading, setIsUnivLoading] = useState(true);
 
-  function getUniversities() {
-    fetch("http://localhost:3001")
+  async function getUniversities() {
+    await fetch("http://localhost:3001")
       .then((response) => {
         return response.text();
       })
       .then((data) => {
-        setUniversities(JSON.parse(data));
-        console.log(JSON.parse(data));
+        const d = JSON.parse(data);
+        setUniversities(d);
+        console.log(d);
+        setIsUnivLoading(false);
+        console.log("Univ Loaded");
       });
   }
 
@@ -35,28 +39,26 @@ export default function Search() {
         }}>
         필터
       </h2>
-      <School />
-      <div
-        style={{
-          minHeight: 100,
-          minWidth: "100vw",
-          border: "1px solid #2AC4C4",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-        {Array.isArray(universities) ? (
-          universities.map((univ, index) => {
-            <div key={index}>
-              <h1>{univ.name_eng}</h1>;
-            </div>;
-          })
-        ) : (
-          <div>
-            <p>No Univ</p>
-          </div>
-        )}
-      </div>
+      {isUnivLoading ? (
+        <p>No Univ</p>
+      ) : (
+        <div
+          style={{
+            minHeight: 100,
+            minWidth: "100vw",
+            border: "1px solid #2AC4C4",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          {universities.map((univ, index) => (
+            <p key={index} style={{ fontSize: "5" }}>
+              {univ.name_eng}
+            </p>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
